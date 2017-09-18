56773faf80bb31197fa07119b46ffd28dc5b76cc
kqo <fritgw@gmail.com>
2017-09-18 16:51:06 +0100

    Add point struct

---
+++ src/point.h
@@ -0,0 +1,37 @@
+struct Point{
+  int x, y;
+
+  Point operator +(const Point& rhs){
+    Point point;
+    point.x = this->x + rhs.x;
+    point.y = this->y + rhs.y;
+    return point;
+  }
+
+  Point operator -(const Point& rhs){
+    Point point;
+    point.x = this->x - rhs.x;
+    point.y = this->y - rhs.y;
+    return point;
+  }
+
+  Point& operator +=(const Point& rhs){
+    this->x += rhs.x;
+    this->y += rhs.y;
+    return *this;
+  }
+
+  Point& operator -=(const Point& rhs){
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+    return *this;
+  }
+
+  bool operator ==(const Point& rhs){
+    return this->x == rhs.x && this->y == rhs.y;
+  }
+
+  bool operator !=(const Point& rhs){
+    return this->x != rhs.x || this->y != rhs.y;
+  }
+};
