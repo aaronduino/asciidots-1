4ed07fe398680a35c5c9d4ca16a16320a48d9ad1
kqo <fritgw@gmail.com>
2017-10-06 20:20:38 +0100

    Add initial dot spawning

--- src/interpret.cc
+++ src/interpret.cc
@@ -6,13 +6,12 @@ using namespace std;
 Interpret::Interpret(string filename){
   circuit = LoadCircuit(filename);
 
-  CircuitTools circuitTools(circuit);
-  cout << circuitTools.FindEscapes(Point{ 2, 0 }).size() << "\n";
+  cout << SpawnDots().size() << "\n";
 }
 
 string* Interpret::LoadCircuit(string filename){
   // get circuit's dimensions
-  uint width, height = 0;
+  width = height = 0;
 
   string line;
   ifstream file(filename);
@@ -29,7 +28,7 @@ string* Interpret::LoadCircuit(string filename){
 
   file.clear();
   file.seekg(0, file.beg); // reset reader
-  
+
   for(int i = 0; i < height; i++){
     getline(file, line);
     circuit[i] = line + string(width - line.length(), ' '); // pad to width
@@ -38,4 +37,43 @@ string* Interpret::LoadCircuit(string filename){
   file.close();
 
   return circuit;
+}
+
+// find spawns, return prepared dots. obviously load circuit first
+vector<Dot> Interpret::SpawnDots()
+{
+  vector<Dot> out;
+
+  // look for spawn chars in circuit
+  for(int y = 0; y < height; y++) for(int x = 0; x < width; x++)
+  {
+    char tile  = circuit[y][x];
+
+    if(tile != '.') // not what we're looking for
+      continue;
+
+    // didn't esc, this could be a spawn
+
+    Point p { x, y };
+    vector<Point> escapes = CircuitTools::FindEscapes(p, circuit);
+
+    switch(escapes.size())
+    {
+      case 0:
+        // spawned dot is doomed TODO: log warn about dead spawn
+        break;
+      case 1:{
+        // unambiguous exit, spawn dot
+        Dot dot(p, escapes[0]);
+        out.push_back(dot);
+
+        break;
+      }
+      default:
+        // ambiguous exit dir TODO: log warn about unresolvable spawn
+        break;
+    }
+  }
+
+  return out;
 }
--- src/interpret.h
+++ src/interpret.h
@@ -2,13 +2,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include "dot.h"
 
 class Interpret{
 public:
   Interpret(std::string filename);
 
 private:
+  uint width, height; // circuit dimensions
   std::string* circuit;
 
   std::string* LoadCircuit(std::string filename);
+  std::vector<Dot> SpawnDots();
 };
