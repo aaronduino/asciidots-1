48d677cff8b2c3335d5e2ceb779363b20809c88a
kqo <fritgw@gmail.com>
2017-10-18 21:45:43 +0100

    Add base for movement

--- src/stepper.cc
+++ src/stepper.cc
@@ -1,7 +1,42 @@
 #include "stepper.h"
+#include "circuittools.h"
+#include "point.h"
 
 using namespace std;
 
-Stepper::Stepper(vector<Dot> dots){
+Stepper::Stepper(vector<string> circuit, vector<Dot> dots){
+  this->circuit = circuit;
   this->dots = dots;
+}
+
+void Stepper::Step(){
+  // do all dots once every tick
+  for(int i = 0; i < dots.size(); i++){
+
+    // kill before sentancing more
+    if(dots[i].deathMarked){
+      dots.erase(dots.begin()+i); // remove i-th element
+      i--; // since removing an element shifts all back by one
+      continue; // dot's dead, next
+    }
+
+    dots[i].Move();
+
+    // for convenience
+    Point pos = dots[i].position;
+    int dir = dots[i].GetDirection();
+
+    if(!CircuitTools::WithinBounds(pos, circuit)){
+      dots[i].deathMarked = true;
+      continue;
+    }
+
+    char tile = circuit[pos.y][pos.x];
+
+    // should we not have entered this tile this way? die
+    if(!TileTools::ValidEntry(tile, dir)){
+      dots[i].deathMarked = true;
+      continue;
+    }
+  }
 }
--- src/stepper.h
+++ src/stepper.h
@@ -1,11 +1,16 @@
+#pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 #include "dot.h"
 
 class Stepper{
 public:
-  Stepper(std::vector<Dot> dots);
+  Stepper(std::vector<std::string> circuit, std::vector<Dot> dots);
 
 private:
   std::vector<Dot> dots; // hold dots with unique IDs
+  std::vector<std::string> circuit;
+
+  void Step();
 };
