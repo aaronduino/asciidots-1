5b136c85282add0154826000f561c35cedc93a47
kqo <fritgw@gmail.com>
2018-02-03 08:01:32 +0000

    Restart again

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,3 @@
-#include <ncurses.h>
-#include "circuit.h"
-#include "debug.h"
-
-int main(int argc, char **argv){
-	// start an ncurses screen
-	initscr();
-
-	// load circuit
-	Circuit circuit(argv[1]);
-	circuit.dots.push_back(Dot(Vec2(0, 0), Vec2(1, 0)));
-
-	// draw circuit
-	draw(circuit);
-
-	// quit on keypress
-	getch();
-
-	// clean up window
-	endwin();
-
+int main(){
 	return 0;
 }
--- src/vec2.cc
+++
@@ -1,9 +0,0 @@
-#include "vec2.h"
-
-Vec2::Vec2(const int &value){
-	x = y = value;
-}
-Vec2::Vec2(const int &xValue, const int &yValue){
-	x = xValue;
-	y = yValue;
-}
