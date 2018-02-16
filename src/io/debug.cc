2e632e3825f8b3e9c0a0af0dc0abace8eb84bf51
kqo <fritgw@gmail.com>
2018-02-16 20:25:30 +0000

    Add outlining to debug windows

--- src/io/debug.cc
+++ src/io/debug.cc
@@ -8,20 +8,23 @@
 WINDOW *wcircuit;
 WINDOW *woutput;
 
-void draw_circuit(const Circuit &circuit);
-void decorate_circuit(const Circuit &circuit);
+void draw_circuit(const Circuit&);
+void decorate_circuit(const Circuit&);
 void draw_output();
+void outline_window(WINDOW*, const std::string&); // outline and title a window
 
 void Debug::init_debug(){
   initscr();
   curs_set(0); // hide cursor
 
-  int y, x;
-  getmaxyx(stdscr, y, x);
+  uint32_t outputLines = 5;
+
+  int height, width; // stdscr bounds
+  getmaxyx(stdscr, height, width);
 
   // windows
-  wcircuit = newwin(y-10, x, 0, 0);
-  woutput = newwin(9, x, y-9, 0);
+  wcircuit = newwin(height - outputLines - 2, width, 0, 0);
+  woutput = newwin(outputLines+2, width, height - outputLines - 2, 0);
 
   // colours
   start_color();
@@ -36,16 +39,12 @@ void Debug::end_debug(){
 void Debug::draw(const Circuit &circuit){
   draw_circuit(circuit); // base layer
   decorate_circuit(circuit); // colour
-
-  wrefresh(wcircuit); // update circuit window
+  outline_window(wcircuit, "circuit");
+  wrefresh(wcircuit); // update circuit window 
 
   draw_output();
-
+  outline_window(woutput, "output");
   wrefresh(woutput);
-
-  move(wcircuit->_maxy+1, 0);
-  hline('-', wcircuit->_maxx+1);
-  refresh();
 }
 
 void draw_circuit(const Circuit &circuit){
@@ -53,7 +52,7 @@ void draw_circuit(const Circuit &circuit){
   for(uint32_t y = 0; y < circuit.height; y++){
     for(uint32_t x = 0; x < circuit.width; x++){
       // load tile, move and draw it
-      mvwaddch(wcircuit, y, x, circuit.get_tile(y, x));
+      mvwaddch(wcircuit, y+1, x+1, circuit.get_tile(y, x));
     }
   }
 }
@@ -65,13 +64,18 @@ void decorate_circuit(const Circuit &circuit){
     y = circuit.dots[i]->pos.y; x = circuit.dots[i]->pos.x;
 
     // replace the tile but with colour
-    mvwaddch(wcircuit, y, x, circuit.get_tile(y, x) | COLOR_PAIR(1));
+    mvwaddch(wcircuit, y+1, x+1, circuit.get_tile(y, x) | COLOR_PAIR(1));
   }
 }
 
 void draw_output(){
   // write in some lines from the output buffer
   for(uint32_t i = 0; i < get_output().size(); i++){
-    mvwaddstr(woutput, i, 0, get_output()[i].c_str());
+    mvwaddstr(woutput, i+1, 1, get_output()[i].c_str());
   }
+}
+
+void outline_window(WINDOW* window, const std::string& title){
+  wborder(window, 0,0,0,0,0,0,0,0);
+  mvwaddstr(window, 0, 2, title.c_str());
 }
--- src/io/debug.h
+++ src/io/debug.h
@@ -6,6 +6,5 @@ class Circuit;
 namespace Debug{
   void init_debug();
   void end_debug();
-
   void draw(const Circuit &circuit); // draw all debug windows
 }
