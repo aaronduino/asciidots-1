c9ea2fd416d9d49429c3c2dfaa6224c5f6fb3209
kqo <fritgw@gmail.com>
2018-02-17 10:19:41 +0000

    Privatise some circuit methods

--- src/logic/circuit.h
+++ src/logic/circuit.h
@@ -14,13 +14,14 @@ public:
 
   void load_circuit(const std::string &path); // load circuit from file
   char get_tile(const int64_t &y, const int64_t &x) const;
-  bool valid_travel(const char &tile, const Vec2 &dir); // can enter tile in dir
-  void spawn_dot(const uint32_t &y, const uint32_t &x);
-  void parse_body(); // scan the body for interesting tiles, create objects
   bool step(); // move all dots, process all interesting tiles
   void post_step(); // handle business after every dot has stepped
-  void process_io(Dot *dot, const char &tile); // handle any reading/writing
 
 private:
   std::vector<std::string> body; // payload of the circuit file
+
+  bool valid_travel(const char &tile, const Vec2 &dir); // can enter tile in dir
+  void spawn_dot(const uint32_t &y, const uint32_t &x);
+  void parse_body(); // scan the body for interesting tiles, create objects
+  void process_io(Dot *dot, const char &tile); // handle any reading/writing
 };
